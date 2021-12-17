83ae614d9d042e87a035556538bd046ba358f660
DanielFB95 <danielfb221195@gmail.com>
2021-12-17 12:39:03 +0100

    creación

---
+++ TrianaTourist/src/test/java/com/trianasalesianos/edu/TrianaTourist/TrianaTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trianasalesianos.edu.TrianaTourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianaTouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
