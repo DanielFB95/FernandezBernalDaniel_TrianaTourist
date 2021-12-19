2e8725a7bf264f74c7e1933af44f5042cdab2036
DanielFB95 <fernandez.bedan20@triana.salesianos.edu>
2021-12-19 19:41:00 +0100

    validador de fotos

--- TrianaTourist/src/main/java/com/trianasalesianos/edu/TrianaTourist/models/dto/PointOfInterestDto.java
+++ TrianaTourist/src/main/java/com/trianasalesianos/edu/TrianaTourist/models/dto/PointOfInterestDto.java
@@ -1,7 +1,5 @@
 package com.trianasalesianos.edu.TrianaTourist.models.dto;
 
-import com.trianasalesianos.edu.TrianaTourist.models.Category;
-import com.trianasalesianos.edu.TrianaTourist.validacion.multiple.anotaciones.FieldsValueMatch;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -9,13 +7,6 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @Builder
-@FieldsValueMatch.List({
-        @FieldsValueMatch(
-                field = "coverPhoto",
-                fieldMatch = "photo2",
-                fieldMatch2 = "photo3"
-        )
-})
 public class PointOfInterestDto {
 
     private String name;
